412d74566176017cd9a77a1ebbbcf07db972eee6
PaolaRaya <pao_rt06@hotmail.com>
2020-06-06 22:57:00 -0500

    Segunda entrega

---
+++ Test/AES.py
@@ -0,0 +1,23 @@
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+with open( 'polirooms.pdf', 'rb' ) as file:
+    fileContent = file.read()
+
+key = get_random_bytes( 32 )
+cipher = AES.new( key , AES.MODE_CTR )
+
+nonce = cipher.nonce
+ciphertext = cipher.encrypt( fileContent )
+
+with open( 'encrypt.pdf' , 'wb' ) as f:
+    f.write( ciphertext )
+
+with open( 'encrypt.pdf' , 'rb' ) as fi:
+    dec = fi.read()
+
+decipher = AES.new(key, AES.MODE_CTR, nonce=nonce)
+aux = decipher.decrypt( dec )
+
+with open( 'recuperando.pdf' , 'wb' ) as f:
+    f.write( aux )

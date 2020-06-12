f9924daf1b6aa9497240cc974db4f7e598a358ad
PaolaRaya <pao_rt06@hotmail.com>
2020-06-12 00:31:14 -0500

    Vista

--- application/forms.py
+++ application/forms.py
@@ -3,6 +3,6 @@ from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username=StringField('Nombre de usuario',validators=[DataRequired()])
-    password= PasswordField('Password',validators=[DataRequired()])
+    username=StringField('Número de Empleado',validators=[DataRequired()])
+    password= PasswordField('Contraseña',validators=[DataRequired()])
     submit= SubmitField('Enviar')

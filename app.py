d0e7ea643efce2f82a1d2337a86afa79c8f8ba15
PaolaRaya <pao_rt06@hotmail.com>
2020-06-11 23:25:49 -0500

    Ordenando again

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from application import create_app
 from flask import render_template, make_response,redirect
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 app = create_app()
 
 @app.route('/')
 @login_required
 def index():
-    return render_template( 'index.html' )
+    username= current_user.id
+    #print("user name: ",username)
+    context={
+        'username':username
+    }
+    return render_template( 'index.html',**context )

72c5a1a731de1217d01d6a8f07f51185b7dbfa2c
RandyVig <rvillanuevag1603@alumno.ipn.mx>
2020-06-13 01:05:43 -0500

    Validando usuario

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,40 +1,35 @@
 from flask import render_template, session, redirect, flash, url_for 
 from application.forms import LoginForm
 from . import auth
-#from app.firestore_service import get_user,user_put
 from application.models import UserModel,UserData
 from flask_login import login_user, login_required, logout_user
+from application.firebase_service import get_user
 #from werkzeug.security import generate_password_hash,check_password_hash
 
 #este es el blueprint
 @auth.route('/login',methods=['GET','POST'])
 def login():
     login_form=LoginForm()
-    context={
+    context = {
         'login_form': login_form
-    }
-    user={
-        'Pao': 'Paola',
-        'Ran': 'Randy',
-        'Bran': 'Brandon'
-    }
-    
-    if login_form.validate_on_submit():
-        
-        username=login_form.username.data
-        password=login_form.password.data
-        try:
-            aux_password = user[username]
-            if( aux_password == password ):
-                flash('Si funciono xd')
-                user_data=UserData(username,password)
+    }   
+    if login_form.validate_on_submit():  
+        user_id = login_form.username.data
+        password = login_form.password.data
+
+        user_doc = get_user( user_id )
+        if user_doc.to_dict() is not None:
+            password_from_db = user_doc.to_dict()['password']
+
+            if password == password_from_db:
+                username = user_doc.to_dict()['user']
+                user_data = UserData(user_id,password)
                 user = UserModel(user_data)
                 login_user(user)
                 return redirect(url_for('index'))
             else:
-                #print('Error contraseña no valida')
                 flash('Contraseña invalida')
-        except KeyError:
-            flash('Usuario no valido')
+        else:
+            flash('El nombre de usuario No existe Intente de nuevo')
 
     return render_template('login.html',**context)
--- application/models.py
+++ application/models.py
@@ -1,4 +1,5 @@
 from flask_login import UserMixin
+from .firebase_service import get_user
 #from .firestore_service import get_user
 user={
     'Pao': 'Paola',
@@ -8,26 +9,19 @@ user={
 
 class UserData:
     def __init__(self,username,password):
-        self.username=username
-        self.password=password
+        self.username = username
+        self.password = password
 
 class UserModel(UserMixin):
     def __init__(self,user_data):
-        '''
-        parametros: UserData
-        '''
-        self.id=user_data.username
-        self.password=user_data.password
+        #parametros: UserData
+        self.id = user_data.username
+        self.password = user_data.password
     
     @staticmethod
     def query(user_id):
-        #buscar user 
-        #convertirlo a user model
-        user_data=UserData(
-            username= user_id,
-            password= user[user_id]
-        )
+        user_doc = get_user( user_id )
+        user_data = UserData( user_doc.to_dict()['user'] , user_doc.to_dict()['password'] )
         #Retornar el nuevo user model
         return UserModel(user_data)
-
 

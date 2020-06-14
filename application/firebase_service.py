2bb8756f0265a2a50c3757cc619893015b39d592
PaolaRaya <pao_rt06@hotmail.com>
2020-06-13 21:43:09 -0500

    Agregando hash doc a firebase2

--- application/firebase_service.py
+++ application/firebase_service.py
@@ -14,8 +14,8 @@ def get_user(user_id):
     return db.collection('users').document(user_id).get()
 
 def put_fileHash(hash,filename,user_id):
-    fileHash_ref = db.collection('files').document(hash).collection('owners').document(user_id)
-    
+    fileHash_ref = db.collection('files').document(hash).collection('owners')
+    fileHash_ref.add({user_id:''})
     #fileHash_ref.add({'id':user_id})
     db.document('files/{}'.format(hash,)).set({'filename':filename})
     #fileHash_ref.set({'filename':filename})
